83a69ef1ddf427fee490b9e43c96f2f994b0fa7f
Sam Olsen <mr.awesome313131@gmail.com>
2019-04-22 12:56:23 -0500

    brick color array update

--- brickBreaker/src/gui/GameWindow.java
+++ brickBreaker/src/gui/GameWindow.java
@@ -2,6 +2,9 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Graphics;
+
+// white = 0 blue = 5 red = 2  orange = 4 yellow = 6 green = 3 purple = 1
 
 public class GameWindow extends JFrame {
 
@@ -15,21 +18,34 @@ public class GameWindow extends JFrame {
                 public void paint(Graphics g)
                 {
                     // set color to red
-                    g.setColor(Color.red);
+                    //g.setColor(Color.red);
 
                     // set Font
-                    g.setFont(new Font("Bold", 1, 20));
+                    //g.setFont(new Font("Bold", 1, 20));
 
                     // draw a string
-                    g.drawString("This is a canvas", 100, 100);
+                    //g.drawString("This is a canvas", 100, 100);
                 }
             };
 
             // set background
-            c.setBackground(Color.blue);
+            c.setBackground(Color.black);
 
             add(c);
-            setSize(1000, 300);
+            setSize(1440, 900);
             show();
     }
+
+    public void level1(){
+        int[][] colors = {{2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4},
+                          {4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2},
+                          {2, 6, 5, 6, 5, 6, 5, 6, 5, 6, 5, 6, 5, 4},
+                          {1, 0, 3, 0, 1, 0, 3, 0, 1, 0, 3, 0, 1, 0},
+                          {0, 1, 0, 3, 0, 1, 0, 3, 0, 1, 0, 3, 0, 1},
+                          {}};
+
+
+    }
+
+
 }

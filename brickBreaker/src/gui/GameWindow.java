b6e7d44f1128cd97ab6305013ba2d2af63ac00df
Olsen <SO137325@davenportschools.org>
2019-04-22 14:03:26 -0500

    attempt at creating blocks

--- brickBreaker/src/gui/GameWindow.java
+++ brickBreaker/src/gui/GameWindow.java
@@ -5,7 +5,8 @@ import java.awt.*;
 import java.awt.Graphics;
 
 // white = 0 blue = 5 red = 2  orange = 4 yellow = 6 green = 3 purple = 1
-
+// length of each rectangle 102.857
+// width of each rectangle 50
 public class GameWindow extends JFrame {
 
     public GameWindow() {
@@ -17,6 +18,12 @@ public class GameWindow extends JFrame {
                 // paint the canvas
                 public void paint(Graphics g)
                 {
+                    for(int i = 0; i < level1.length; i++){
+                        for(int j = 0; j < colors.length[0]; j++){
+
+                        }
+                    }
+
                     // set color to red
                     //g.setColor(Color.red);
 
@@ -36,13 +43,14 @@ public class GameWindow extends JFrame {
             show();
     }
 
-    public void level1(){
+    public int[][] level1(){
         int[][] colors = {{2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4},
                           {4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2},
                           {2, 6, 5, 6, 5, 6, 5, 6, 5, 6, 5, 6, 5, 4},
                           {1, 0, 3, 0, 1, 0, 3, 0, 1, 0, 3, 0, 1, 0},
                           {0, 1, 0, 3, 0, 1, 0, 3, 0, 1, 0, 3, 0, 1},
-                          {}};
+                          {2, 6, 5, 6, 5, 6, 5, 6, 5, 6, 5, 6, 5, 4}};
+        return colors;
 
 
     }
